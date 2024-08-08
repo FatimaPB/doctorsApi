238b1f345526e730076b5079c839374c06a28b38
Fátima PB <20221024@uthh.edu.mx>
2024-08-08 01:20:34 -0600

    gfdc

--- src/routes/Recuperacion.js
+++ src/routes/Recuperacion.js
@@ -22,32 +22,31 @@ router.post('/verificar-correo-respuesta', async (req, res) => {
     res.status(500).json({ error: 'Error al verificar el correo o la respuesta.' });
   }
 });
-
 // Ruta para establecer una nueva contraseña
 router.post('/establecer-nueva-contrasena', async (req, res) => {
-  const { correo, nuevaContrasena } = req.body;
-
-  try {
-    // Buscar al cliente por correo
-    const cliente = await Cliente.findOne({ correo });
-
-    if (!cliente) {
-      return res.status(404).json({ error: 'Cliente no encontrado.' });
+    const { correo, nuevaContrasena } = req.body;
+  
+    try {
+      // Buscar al cliente por correo
+      const cliente = await Cliente.findOne({ correo });
+  
+      if (!cliente) {
+        return res.status(404).json({ error: 'Cliente no encontrado.' });
+      }
+  
+      // Generar el hash de la nueva contraseña
+      const hash = await bcrypt.hash(nuevaContrasena, 10);
+  
+      // Actualizar la contraseña del cliente en la base de datos
+      cliente.contrasena = hash;
+      await cliente.save();
+  
+      // Responder al cliente con éxito
+      res.status(200).json({ message: 'Contraseña actualizada correctamente.' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error al intentar actualizar la contraseña.' });
     }
-
-    // Generar el hash de la nueva contraseña
-    const hash = await hash(nuevaContrasena, 10);
-
-    // Actualizar la contraseña del cliente en la base de datos
-    cliente.contrasena = hash;
-    await cliente.save();
-
-    // Responder al cliente con éxito
-    res.status(200).json({ message: 'Contraseña actualizada correctamente.' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Error al intentar actualizar la contraseña.' });
-  }
-});
+  });
 
 module.exports = router;
