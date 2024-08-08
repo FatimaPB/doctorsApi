c9f73d939e24bef04eb616f9f4aefa8fed1f6225
Fátima PB <20221024@uthh.edu.mx>
2024-08-08 01:26:50 -0600

    hshsy

--- src/routes/Recuperacion.js
+++ src/routes/Recuperacion.js
@@ -1,8 +1,7 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const router = express.Router();
 const Cliente = require('../models/Cliente');
-const bcrypt = require('bcrypt');
-
 
 // Ruta para verificar correo y respuesta
 router.post('/verificar-correo-respuesta', async (req, res) => {
@@ -23,31 +22,32 @@ router.post('/verificar-correo-respuesta', async (req, res) => {
     res.status(500).json({ error: 'Error al verificar el correo o la respuesta.' });
   }
 });
+
 // Ruta para establecer una nueva contraseña
 router.post('/establecer-nueva-contrasena', async (req, res) => {
-    const { correo, nuevaContrasena } = req.body;
-  
-    try {
-      // Buscar al cliente por correo
-      const cliente = await Cliente.findOne({ correo });
-  
-      if (!cliente) {
-        return res.status(404).json({ error: 'Cliente no encontrado.' });
-      }
-  
-      // Generar el hash de la nueva contraseña
-      const hash = await bcrypt.hash(nuevaContrasena, 10);
-  
-      // Actualizar la contraseña del cliente en la base de datos
-      cliente.contrasena = hash;
-      await cliente.save();
-  
-      // Responder al cliente con éxito
-      res.status(200).json({ message: 'Contraseña actualizada correctamente.' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Error al intentar actualizar la contraseña.' });
+  const { correo, nuevaContrasena } = req.body;
+
+  try {
+    // Buscar al cliente por correo
+    const cliente = await Cliente.findOne({ correo });
+
+    if (!cliente) {
+      return res.status(404).json({ error: 'Cliente no encontrado.' });
     }
-  });
+
+    // Generar el hash de la nueva contraseña
+    const hash = await bcrypt.hash(nuevaContrasena, 10);
+
+    // Actualizar la contraseña del cliente en la base de datos
+    cliente.contrasena = hash;
+    await cliente.save();
+
+    // Responder al cliente con éxito
+    res.status(200).json({ message: 'Contraseña actualizada correctamente.' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al intentar actualizar la contraseña.' });
+  }
+});
 
 module.exports = router;
